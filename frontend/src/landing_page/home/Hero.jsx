16c8dc3de7abaf7ce3f4b57b14181f873c4cf6b8
vivek210404 <vivek210404@gmail.com>
2025-10-02 17:36:19 +0530

    Fixed signup button action everywhere

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Hero() {
     return ( 
@@ -7,7 +8,11 @@ function Hero() {
                 <img src="/images/homeHero.png" alt="Hero Image" className='mb-5' />
                 <h1 className='mt-5'>Invest in everything</h1>
                 <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"25%", margin:"0 auto"}}>Sign up for free</button>
+                <Link to="/signup" style={{width:"25%", margin:"0 auto"}}>
+                    <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"100%"}}>
+                        Sign up for free
+                    </button>
+                </Link>
             </div>
         </div>
      );
--- frontend/src/landing_page/products/Universe.jsx
+++ frontend/src/landing_page/products/Universe.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Universe() {
   return (
@@ -11,14 +12,22 @@ function Universe() {
         </p>
 
         <div className="col-4 p-3 mt-5">
-          <img src="/images/zerodhaFundhouse.png" alt="" style={{width:"90%"}}/>
+          <img
+            src="/images/zerodhaFundhouse.png"
+            alt=""
+            style={{ width: "90%" }}
+          />
           <p className="text-small text-muted">
             Our asset management venture that is creating simple and transparent
             index funds to help you save for your goals.
           </p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="/images/sensibullLogo.svg" alt="" style={{width:"90%"}}/>
+          <img
+            src="/images/sensibullLogo.svg"
+            alt=""
+            style={{ width: "90%" }}
+          />
           <p className="text-small text-muted">
             Options trading platform that lets you create strategies, analyze
             positions, and examine data points like open interest, FII/DII, and
@@ -26,7 +35,7 @@ function Universe() {
           </p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="/images/tijori.svg" alt="" style={{width:"90%"}}/>
+          <img src="/images/tijori.svg" alt="" style={{ width: "90%" }} />
           <p className="text-small text-muted">
             Investment research platform that offers detailed insights on
             stocks, sectors, supply chains, and more.
@@ -34,29 +43,38 @@ function Universe() {
         </div>
 
         <div className="col-4 p-3 mt-5">
-          <img src="/images/streakLogo.png" alt="" style={{width:"90%"}}/>
+          <img src="/images/streakLogo.png" alt="" style={{ width: "90%" }} />
           <p className="text-small text-muted">
             Systematic trading platform that allows you to create and backtest
             strategies without coding.
           </p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="/images/smallcaseLogo.png" alt="" style={{width:"90%"}}/>
+          <img
+            src="/images/smallcaseLogo.png"
+            alt=""
+            style={{ width: "90%" }}
+          />
           <p className="text-small text-muted">
             Thematic investing platform that helps you invest in diversified
             baskets of stocks on ETFs.
           </p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="/images/dittoLogo.png" alt="" style={{width:"90%"}}/>
+          <img src="/images/dittoLogo.png" alt="" style={{ width: "90%" }} />
           <p className="text-small text-muted">
             Personalized advice on life and health insurance. No spam and no
             mis-selling.
           </p>
         </div>
-        <button className="p-2 btn btn-primary fs-5 mb-5" style={{ width: "25%", margin: "0 auto" }}>
-          Sign up for free
-        </button>
+        <Link to="/signup" style={{ width: "25%", margin: "0 auto" }}>
+          <button
+            className="p-2 btn btn-primary fs-5 mb-5"
+            style={{ width: "100%" }}
+          >
+            Sign up for free
+          </button>
+        </Link>
       </div>
     </div>
   );
